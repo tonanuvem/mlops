115c5c32e51fa89620cbceb96a5fe32b3bd80e9a
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-22 00:54:38 -0300

    Update volume.sh

--- volume.sh
+++ volume.sh
@@ -1,4 +1,6 @@
 kubectl get pv | grep fiap
+VOL=$(kubectl get pv | grep fiap | awk '{print \$1}' | xargs echo)
 
 PX_POD=$(kubectl get pods -l name=portworx -n kube-system -o jsonpath='{.items[0].metadata.name}')
 kubectl exec -it $PX_POD -n kube-system -- /opt/pwx/bin/pxctl volume list
+kubectl exec -it $PX_POD -n kube-system -- /opt/pwx/bin/pxctl volume inspect $VOL
