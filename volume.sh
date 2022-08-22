cfe503983f025cca775d6136a79da812dec9a28c
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-22 00:57:22 -0300

    Update volume.sh

--- volume.sh
+++ volume.sh
@@ -1,9 +1,8 @@
+echo ""
 echo "Informações sobre persistent volume no Cluster:"
 kubectl get pv | grep fiap
 echo ""
-echo ""
 VOL=$(kubectl get pv | grep fiap | awk '{print $1}' | xargs echo)
-echo ""
 PX_POD=$(kubectl get pods -l name=portworx -n kube-system -o jsonpath='{.items[0].metadata.name}')
 #kubectl exec -it $PX_POD -n kube-system -- /opt/pwx/bin/pxctl volume list
 echo ""
