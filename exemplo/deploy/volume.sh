2144cd39f500e7188245ad12399c00563b8e98a7
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-21 23:11:36 -0300

    Update volume.sh

--- exemplo/deploy/volume.sh
+++ exemplo/deploy/volume.sh
@@ -1,2 +1,2 @@
 PX_POD=$(kubectl get pods -l name=portworx -n kube-system -o jsonpath='{.items[0].metadata.name}')
-kubectl exec -it $PX_POD -n kube-system -- /opt/pwx/bin/pxctl status
+kubectl exec -it $PX_POD -n kube-system -- /opt/pwx/bin/pxctl list
