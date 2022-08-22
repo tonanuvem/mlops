bae69bb32b0c6782aff5e160df8c4a0a5f619e3c
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-22 01:26:41 -0300

    Update volume.sh

--- volume.sh
+++ volume.sh
@@ -4,6 +4,9 @@ kubectl get pv | grep fiap
 echo ""
 VOL=$(kubectl get pv | grep fiap | awk '{print $1}' | xargs echo)
 PX_POD=$(kubectl get pods -l name=portworx -n kube-system -o jsonpath='{.items[0].metadata.name}')
+echo ""
+echo "Detalhe do persistent volume:"
+kubectl describe pv $VOL
 #kubectl exec -it $PX_POD -n kube-system -- /opt/pwx/bin/pxctl volume list
 echo ""
 echo "Informações sobre volume:"
