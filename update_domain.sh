92dd2e44b4292200da53674b28e6baf1fbfd3f86
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-21 22:50:14 -0300

    Update update_domain.sh

--- update_domain.sh
+++ update_domain.sh
@@ -2,4 +2,4 @@ export INGRESS_HOST=$(curl checkip.amazonaws.com)
 export INGRESS_DOMAIN=${INGRESS_HOST}.nip.io
 echo $INGRESS_DOMAIN
 sed -i 's|nip.io|'$INGRESS_DOMAIN'|' ~/kubeflow/config-domain.yaml
-kubectl apply --filename ~/kubeflow/config-domain.yaml
+kubectl apply --filename ~/mlops/config-domain.yaml
