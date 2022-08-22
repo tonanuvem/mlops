2b656794b6bc458e3f0456babe8c94b3da1fb5a4
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-22 00:49:21 -0300

    Update and rename exemplo/deploy/volume.sh to volume.sh

--- volume.sh
+++ volume.sh
@@ -1,2 +1,4 @@
+kubectl get pv | grep fiap
+
 PX_POD=$(kubectl get pods -l name=portworx -n kube-system -o jsonpath='{.items[0].metadata.name}')
 kubectl exec -it $PX_POD -n kube-system -- /opt/pwx/bin/pxctl volume list
