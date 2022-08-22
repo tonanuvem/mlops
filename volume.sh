c415ca017574596f1f2cb07bf0c3db50a01a1df9
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-22 09:26:48 -0300

    Update volume.sh

--- volume.sh
+++ volume.sh
@@ -1,19 +1,19 @@
 echo ""
-echo "Informações sobre PERSISTENT VOLUME CLAIM (PVC) e STORAGE CLASS (SC):"
+echo "   Informações sobre PERSISTENT VOLUME CLAIM (PVC) e STORAGE CLASS (SC):"
 echo ""
 kubectl get pvc -n fiap
 echo ""
-echo "Informações sobre PERSISTENT VOLUME (PV) no Cluster:"
+echo "   Informações sobre PERSISTENT VOLUME (PV) no Cluster:"
 echo ""
 kubectl get pv | grep fiap
 echo ""
 VOL=$(kubectl get pv | grep fiap | awk '{print $1}' | xargs echo)
 PX_POD=$(kubectl get pods -l name=portworx -n kube-system -o jsonpath='{.items[0].metadata.name}')
 #echo ""
-#echo "Detalhe do PERSISTENT VOLUME (PV), informando quem é o STORAGE CLASS (SC) usado:"
+#echo "   Detalhe do PERSISTENT VOLUME (PV), informando quem é o STORAGE CLASS (SC) usado:"
 #kubectl describe pv $VOL
 #kubectl exec -it $PX_POD -n kube-system -- /opt/pwx/bin/pxctl volume list
 echo ""
-echo "Informações sobre o DISCO GERENCIADO pelo portworx::"
+echo "   Informações sobre o DISCO GERENCIADO pelo portworx::"
 echo ""
 kubectl exec -it $PX_POD -n kube-system -- /opt/pwx/bin/pxctl volume inspect $VOL
