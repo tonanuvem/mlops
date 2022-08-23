a557cbaf698de51eeedaef064cbebf77ba459b74
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-23 11:30:14 -0300

    Update predict.py

--- exemplo/deploy/predict.py
+++ exemplo/deploy/predict.py
@@ -62,4 +62,4 @@ def home():
     #return "API ONLINE. Acessar o endpoint: /predict"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
