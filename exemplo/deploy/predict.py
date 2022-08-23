d495b8966256c2c31928c5e33c07af0b1769d9be
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-23 09:58:07 -0300

    Update predict.py

--- exemplo/deploy/predict.py
+++ exemplo/deploy/predict.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask, jsonify, request, render_template
 from flask_restful import Api, Resource
+from flask_cors import CORS, cross_origin
 import joblib
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 
 if not os.path.isfile('model.joblib'):
@@ -14,11 +16,14 @@ model = joblib.load('model.joblib')
 
 class MakePrediction(Resource):
     @staticmethod
+    @cross_origin()
     def post():
         payload = request.get_json()
         features = payload['fields']
-        dados = payload['values'][0]
-
+        dados = payload['values']#[0]
+        print('Dados recebidos: ')
+        print(str(dados)) 
+        
         REPROVACOES_MAT1 = dados[0] #['REPROVACOES_MAT']
         REPROVACOES_MAT2 = dados[1] #['REPROVACOES_MAT']
         REPROVACOES_MAT3 = dados[2] #['REPROVACOES_MAT']
@@ -41,6 +46,7 @@ class MakePrediction(Resource):
         })
     
     @staticmethod
+    @cross_origin()
     def get():
         return "API do Modelo esta online. Para fazer uma previsao, deve-se enviar um POST."
 
@@ -53,4 +59,4 @@ def home():
     #return "API ONLINE. Acessar o endpoint: /predict"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
