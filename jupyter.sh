d433973c9059cc887eab10f5108886e845437c69
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-08-14 17:23:40 -0300

    Create jupyter.sh

---
+++ jupyter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Executando Jupyter Notebook para análise e ML dos dados"
+
+dir=`pwd`; dir="$(dirname "$dir")"; echo $dir;
+
+#https://jupyter-server.readthedocs.io/en/latest/operators/public-server.html#preparing-a-hashed-password
+# jupyter/scipy-notebook
+# jupyter/datascience-notebook
+
+docker run -it --name jupyter --rm -p 8888:8888 -v "$dir":/home/jovyan/work -v "$dir/exemplo/treinamento.ipynb:/home/jovyan/treinamento.ipynb" -d jupyter/scipy-notebook \
+    start-notebook.sh --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$cIQ7S1OapqyvWzmH636CsA$fh0xOdGwdwv6/cxW5Bqi2mPZuSZlG0zGLwcxl7Ulvac'
+
+echo ""
+echo "URL de acesso:"
+echo ""
+echo http://$(curl -s checkip.amazonaws.com):8888
+echo ""
+echo "   Senha: admin"
+echo ""
