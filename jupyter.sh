23ac6a07bc701a2eaa061c7ac5e7eaa271302064
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-08-14 17:34:50 -0300

    Update jupyter.sh

--- jupyter.sh
+++ jupyter.sh
@@ -8,7 +8,7 @@ dir=`pwd`; dir="$(dirname "$dir")"; echo $dir;
 # jupyter/scipy-notebook
 # jupyter/datascience-notebook
 
-docker run -it --name jupyter --rm -p 8888:8888 -v "$dir"/mlops/exemplo:/home/jovyan/work -v "$dir/mlpos/exemplo/treinamento.ipynb:/home/jovyan/treinamento.ipynb" -d jupyter/scipy-notebook \
+docker run -it --name jupyter --rm -p 8888:8888 -v "$dir"/mlops/exemplo:/home/jovyan/work -v $dir/mlpos/exemplo/treinamento.ipynb:/home/jovyan/treinamento.ipynb -d jupyter/scipy-notebook \
     start-notebook.sh --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$cIQ7S1OapqyvWzmH636CsA$fh0xOdGwdwv6/cxW5Bqi2mPZuSZlG0zGLwcxl7Ulvac'
 
 echo ""
