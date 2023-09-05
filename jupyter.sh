cd52174ffd300a3900b70d9ea55d1b7463d7ace5
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-09-05 08:49:26 -0300

    Update jupyter.sh

--- jupyter.sh
+++ jupyter.sh
@@ -3,6 +3,7 @@
 echo "Executando Jupyter Notebook para análise e ML dos dados"
 
 dir=`pwd`; dir="$(dirname "$dir")"; echo $dir;
+# dir="$(pwd)"; echo $dir;
 
 #https://jupyter-server.readthedocs.io/en/latest/operators/public-server.html#preparing-a-hashed-password
 # jupyter/scipy-notebook
