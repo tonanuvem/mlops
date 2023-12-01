7b23203e3dd65b51f1109efce1b7a912baa82ec5
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-12-01 20:16:00 -0300

    Update azure_install_kubeflow.sh

--- azure_install_kubeflow.sh
+++ azure_install_kubeflow.sh
@@ -19,29 +19,29 @@ while ! ./kustomize build vanilla | kubectl apply -f -; do echo "Retrying to app
 #./kustomize build vanilla > kubeflow_vanilla.yaml
 #kubectl apply -f kubeflow_vanilla.yaml
 
-echo "Verificando os pods em execução dos namespaces."
+# https://stackoverflow.com/questions/76793434/kubeflow-jupyter-notebook-error-could-not-find-csrf-cookie-xsrf-token-in-the-req
+# https://github.com/kubeflow/manifests/issues/2225
+kubectl patch deploy jupyter-web-app-deployment -n kubeflow -p '{"spec": {"type": "LoadBalancer"}}'
 
-kubectl get pods -n cert-manager
-kubectl get pods -n istio-system
-kubectl get pods -n auth
-kubectl get pods -n knative-eventing
-kubectl get pods -n knative-serving
-kubectl get pods -n kubeflow
-kubectl get pods -n kubeflow-user-example-com
+#echo "Verificando os pods em execução dos namespaces."
+#kubectl get pods -n cert-manager
+#kubectl get pods -n istio-system
+#kubectl get pods -n auth
+#kubectl get pods -n knative-eventing
+#kubectl get pods -n knative-serving
+#kubectl get pods -n kubeflow
+#kubectl get pods -n kubeflow-user-example-com
 
-echo "Verificando os Gateways criados (Ingress)."
+#echo "Verificando os Gateways criados (Ingress)."
+#kubectl get gateway -n kubeflow
 
-kubectl get gateway -n kubeflow
+#echo "Verificando os Virtual Services criados."
+#kubectl get destinationrule -n kubeflow
 
-echo "Verificando os Virtual Services criados."
+#echo "Verificando os Virtual Services criados."
+#kubectl get destinationrule -n kubeflow
 
-kubectl get destinationrule -n kubeflow
 
-echo "Verificando os Virtual Services criados."
-
-kubectl get destinationrule -n kubeflow
-
-echo " > Acessar o UI do Kubeflow"
 kubectl patch svc istio-ingressgateway -n istio-system -p '{"spec": {"type": "LoadBalancer"}}'
 
 echo "Verificando se o PROFILE está RUNNING : $ kubectl get pod -n kubeflow | grep profile"
@@ -63,13 +63,12 @@ echo "INGRESS_DOMAIN = $INGRESS_DOMAIN"
 # (including the default password 12341234 if not changed) will be used from the time the Service is exposed via LoadBalancer until the time this command is run or the dex is otherwise restarted.
 kubectl rollout restart deployment dex -n auth
 
-user@example.com and the default password is 12341234
-
+echo " > Acessar o UI do Kubeflow"
 echo ""
 echo " -- "
 echo ""
 # Acesso ao Kubeflow
-echo " Acessar Istio Kiali: http://$INGRESS_DOMAIN"
+echo " Acessar Kubeflow: http://$INGRESS_DOMAIN"
 echo "    login = user@example.com"
 echo "    pass  = 12341234"
 echo ""
