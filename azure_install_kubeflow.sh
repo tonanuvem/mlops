5b76a1a55ac802d487d2dd0adee6537931bf08d6
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-12-02 00:37:24 -0300

    Update azure_install_kubeflow.sh

--- azure_install_kubeflow.sh
+++ azure_install_kubeflow.sh
@@ -27,16 +27,57 @@ alias kustomize="$(pwd)/kustomize"
 #kubectl apply -f kubeflow_vanilla.yaml
 
 # 3) rodar somente alguns componentes para econmicar cpu e memoria
+# Cert-manager
+kustomize build common/cert-manager/cert-manager/base | kubectl apply -f -
+kubectl wait --for=condition=ready pod -l 'app in (cert-manager,webhook)' --timeout=180s -n cert-manager
+kustomize build common/cert-manager/kubeflow-issuer/base | kubectl apply -f -
+# Istio
+kustomize build common/istio-1-16/istio-crds/base | kubectl apply -f -
+kustomize build common/istio-1-16/istio-namespace/base | kubectl apply -f -
+kustomize build common/istio-1-16/istio-install/base | kubectl apply -f -
+# Dex
+kustomize build common/dex/overlays/istio | kubectl apply -f -
+# OIDC Auth
 kustomize build common/oidc-authservice/base | kubectl apply -f -
+# Kubeflow Namespace
 kustomize build common/kubeflow-namespace/base | kubectl apply -f -
+# Kubeflow Roles
 kustomize build common/kubeflow-roles/base | kubectl apply -f -
+# Kubeflow Istio Resources
 kustomize build common/istio-1-16/kubeflow-istio-resources/base | kubectl apply -f -
+# Central Dashboard
 kustomize build apps/centraldashboard/upstream/overlays/kserve | kubectl apply -f -
+# Admission Webhook
 kustomize build apps/admission-webhook/upstream/overlays/cert-manager | kubectl apply -f -
+# Notebooks
 kustomize build apps/jupyter/notebook-controller/upstream/overlays/kubeflow | kubectl apply -f -
 kustomize build apps/jupyter/jupyter-web-app/upstream/overlays/istio | kubectl apply -f -
+# Profiles + KFAM
 kustomize build apps/profiles/upstream/overlays/kubeflow | kubectl apply -f -
+# Volumes Web App
+kustomize build apps/volumes-web-app/upstream/overlays/istio | kubectl apply -f -
+# User Namespace
 kustomize build common/user-namespace/base | kubectl apply -f -
+
+# [OPCIONAL] Knative
+kustomize build common/knative/knative-serving/overlays/gateways | kubectl apply -f -
+kustomize build common/istio-1-16/cluster-local-gateway/base | kubectl apply -f -
+kustomize build common/knative/knative-eventing/base | kubectl apply -f -
+# [OPCIONAL] Kubeflow Pipelines
+kustomize build apps/pipeline/upstream/env/cert-manager/platform-agnostic-multi-user | awk '!/well-defined/' | kubectl apply -f -
+# [OPCIONAL] KServe (usa um pouco mais de memoria)
+kustomize build contrib/kserve/kserve | kubectl apply -f -
+kustomize build contrib/kserve/models-web-app/overlays/kubeflow | kubectl apply -f -
+kubectl get cm config-domain --namespace knative-serving 
+
+# [OPCIONAL] Katlib (usa bastante memoria, não rodar esse componentes)
+#kustomize build apps/katib/upstream/installs/katib-with-kubeflow | kubectl apply -f -
+# [OPCIONAL] Tensorboard
+kustomize build apps/tensorboard/tensorboards-web-app/upstream/overlays/istio | kubectl apply -f -
+kustomize build apps/tensorboard/tensorboard-controller/upstream/overlays/kubeflow | kubectl apply -f -
+# OPCIONAL] Training Operator
+kustomize build apps/training-operator/upstream/overlays/kubeflow | kubectl apply -f -
+
 
 # https://stackoverflow.com/questions/76793434/kubeflow-jupyter-notebook-error-could-not-find-csrf-cookie-xsrf-token-in-the-req
 # https://github.com/kubeflow/manifests/issues/2225
