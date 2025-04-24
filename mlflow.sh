4e8c0b01cf1276bb4c6ada26f9038da097a0fc21
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-04-24 11:32:56 -0300

    Update mlflow.sh

--- mlflow.sh
+++ mlflow.sh
@@ -1,10 +1,18 @@
+# Pre-reqs:
+
+sudo yum install -y swig python3-devel
+
+# Executando local
+
 python3 -m venv mlflow
-
 source mlflow/bin/activate
-
 pip install mlflow
-
 mlflow server --host 0.0.0.0 --port 8089 &
+
+# Executando com docker
+# docker run -it --name mlflow -p 8089:5000 -d bitnami/mlflow:2.22.0
+docker run -it -v ${PWD}/mlops/exemplo/mlflow:/opt/nb -p 8888:8888 mfeurer/auto-sklearn:master /bin/bash -c "mkdir -p /opt/nb && jupyter notebook --notebook-dir=/opt/nb --ip='0.0.0.0' --port=8888 --no-browser --allow-root"
+
 IP=$(curl -s checkip.amazonaws.com)
 
 echo ""
