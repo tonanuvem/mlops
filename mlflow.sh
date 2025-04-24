9d6ba11966144d4862f359a697af10598299c045
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-04-24 17:38:59 -0300

    Update mlflow.sh

--- mlflow.sh
+++ mlflow.sh
@@ -11,7 +11,7 @@ mlflow server --host 0.0.0.0 --port 8089 &
 
 # Executando com docker
 # docker run -it --name mlflow -p 8089:5000 -d bitnami/mlflow:2.22.0
-docker run -it -v ${PWD}/mlops/exemplo:/opt/nb -p 8889:8888 -d mfeurer/auto-sklearn:master /bin/bash -c "mkdir -p /opt/nb && jupyter notebook --notebook-dir=/opt/nb --ip='0.0.0.0' --port=8888 --no-browser --allow-root"
+docker run -it -v ${PWD}/mlops/exemplo:/opt/nb -p 8789:8888 -d mfeurer/auto-sklearn:master /bin/bash -c "mkdir -p /opt/nb && jupyter notebook --notebook-dir=/opt/nb --ip='0.0.0.0' --port=8888 --no-browser --allow-root"
 
 IP=$(curl -s checkip.amazonaws.com)
 
@@ -22,4 +22,7 @@ echo ""
 echo ""
 echo "URLs do projeto:"
 echo ""
+echo " - JUPYTER AUTO ML          : http://$IP:8789 (inserir o token que aparece acima)"
+echo ""
 echo " - MLFLOW UI                : http://$IP:8089"
+echo ""
