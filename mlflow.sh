1067a9f1cfe47c44e0fbc9960bc703971f146e7a
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-04-24 18:14:20 -0300

    Update mlflow.sh

--- mlflow.sh
+++ mlflow.sh
@@ -11,9 +11,10 @@ mlflow server --host 0.0.0.0 --port 8089 &
 
 # Executando com docker
 # docker run -it --name mlflow -p 8089:5000 -d bitnami/mlflow:2.22.0
-docker run -it -v ${PWD}/mlops/exemplo/mlflow:/opt/nb -p 8789:8888 -d mfeurer/auto-sklearn:master /bin/bash -c "mkdir -p /opt/nb && jupyter notebook --notebook-dir=/opt/nb --ip='0.0.0.0' --port=8888 --no-browser --allow-root"
+docker run -it --name automl -v ${PWD}/mlops/exemplo/mlflow:/opt/nb -p 8789:8888 -d mfeurer/auto-sklearn:master /bin/bash -c "mkdir -p /opt/nb && jupyter notebook --notebook-dir=/opt/nb --ip='0.0.0.0' --port=8888 --no-browser --allow-root"
 
 IP=$(curl -s checkip.amazonaws.com)
+TOKEN=$(docker logs automl | grep token | grep 127. | grep NotebookApp | sed -n 's/.*?token=\([a-f0-9]*\).*/\1/p')
 
 echo ""
 echo ""
@@ -22,7 +23,7 @@ echo ""
 echo ""
 echo "URLs do projeto:"
 echo ""
-echo " - JUPYTER AUTO ML          : http://$IP:8789 (inserir o token que aparece acima)"
+echo " - JUPYTER AUTO ML          : http://$IP:8789/?token=$TOKEN"
 echo ""
 echo " - MLFLOW UI                : http://$IP:8089"
 echo ""
