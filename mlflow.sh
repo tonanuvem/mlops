a13098bcbf9dd8a94e2658bc96f4aafc10962ba8
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-04-25 09:14:26 -0300

    Update mlflow.sh

--- mlflow.sh
+++ mlflow.sh
@@ -14,6 +14,12 @@ mlflow server --host 0.0.0.0 --port 8089 &
 docker run -it --name automl -v ${PWD}/mlops/exemplo/mlflow:/opt/nb -p 8789:8888 -d mfeurer/auto-sklearn:master /bin/bash -c "mkdir -p /opt/nb && jupyter notebook --notebook-dir=/opt/nb --ip='0.0.0.0' --port=8888 --no-browser --allow-root"
 
 IP=$(curl -s checkip.amazonaws.com)
+echo "Aguardando TOKEN (geralmente 1 min)"
+while [ "$(docker logs automl | grep token | grep 127. | grep NotebookApp | wc -l)" != "1" ]; do
+  printf "."
+  sleep 1
+done
+echo "Token Pronto."
 TOKEN=$(docker logs automl | grep token | grep 127. | grep NotebookApp | sed -n 's/.*?token=\([a-f0-9]*\).*/\1/p')
 
 echo ""
