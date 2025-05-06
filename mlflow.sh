45327e5c3114957a4d50f9566b6330a71eca44d9
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-05-06 08:56:30 -0300

    Update mlflow.sh

--- mlflow.sh
+++ mlflow.sh
@@ -1,6 +1,6 @@
 # Pre-reqs:
 
-sudo yum install -y swig python3-devel
+# sudo yum install -y swig python3-devel
 
 # Executando local
 
@@ -11,16 +11,6 @@ mlflow server --host 0.0.0.0 --port 8089 &
 
 # Executando com docker
 # docker run -it --name mlflow -p 8089:5000 -d bitnami/mlflow:2.22.0
-docker run -it --name automl -v ${PWD}/mlops/exemplo/mlflow:/opt/nb -p 8789:8888 -d mfeurer/auto-sklearn:master /bin/bash -c "mkdir -p /opt/nb && jupyter notebook --notebook-dir=/opt/nb --ip='0.0.0.0' --port=8888 --no-browser --allow-root"
-
-IP=$(curl -s checkip.amazonaws.com)
-echo "Aguardando TOKEN (geralmente 1 min)"
-while [ "$(docker logs automl | grep token | grep 127. | grep NotebookApp | wc -l)" != "1" ]; do
-  printf "."
-  sleep 1
-done
-echo "Token Pronto."
-TOKEN=$(docker logs automl | grep token | grep 127. | grep NotebookApp | sed -n 's/.*?token=\([a-f0-9]*\).*/\1/p')
 
 echo ""
 echo ""
@@ -28,8 +18,6 @@ echo "Config OK"
 echo ""
 echo ""
 echo "URLs do projeto:"
-echo ""
-echo " - JUPYTER AUTO ML          : http://$IP:8789/?token=$TOKEN"
 echo ""
 echo " - MLFLOW UI                : http://$IP:8089"
 echo ""
